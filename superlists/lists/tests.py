936dd0f6e42cc0137cae2f964b553c95addc194b
samirtf <samircc20092@gmail.com>
2020-01-15 23:50:01 -0300

    Test only save when comes a POST request

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -21,6 +21,9 @@ class HomePageTest(TestCase):
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_only_saves_itens_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModeTest(TestCase):
@@ -41,3 +44,14 @@ class ItemModeTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
